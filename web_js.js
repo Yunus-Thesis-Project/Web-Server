e261321f2686951506f183654d67a1698985bc61
Yunus Bayu Aji <yunus.bayu.aji@mail.ugm.ac.id>
2021-03-02 17:31:33 +0700

    update web

--- web_js.js
+++ web_js.js
@@ -2,7 +2,7 @@ function validation(){
     var username = document.loginform.uname.value;
     var password = document.loginform.passwd.value;
     if(username == "ajietb" && password == "skripsi"){
-        alert("Password Benar");
+        location.href = "web_data.html"
         return true;
     }else if(username=="" && password==""){
         alert("Masukkan Username dan Password");
@@ -12,3 +12,14 @@ function validation(){
         return false;
     }
 }
+
+var listener = new ROSLIB.Topic({
+    ros : ros,
+    name : '/chatter',
+    messageType : 'std_msgs/String'
+});
+
+listener.subscribe(function(m) {
+    document.getElementById("value_1").innerHTML = "Waduh";
+    listener.unsubscribe();
+});
