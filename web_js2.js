46a941dc1d8533dfc8c5c34ab7b63ad06764e92d
Yunus Bayu Aji <yunus.bayu.aji@mail.ugm.ac.id>
2021-03-03 16:49:10 +0700

    update data css

---
+++ web_js2.js
@@ -0,0 +1,44 @@
+var ros = new ROSLIB.Ros({
+    url : 'ws://192.168.43.244:9090'
+});
+
+ros.on('connection', function() {
+console.log('Connected to websocket server.');
+});
+
+ros.on('error', function(error) {
+console.log('Error connecting to websocket server: ', error);
+});
+
+ros.on('close', function() {
+console.log('Connection to websocket server closed.');
+});
+
+var imageTopic= new ROSLIB.Topic({
+ros : ros,
+name : '/image',
+messageType : 'sensor_msgs/CompressedImage'
+});
+
+var listener = new ROSLIB.Topic({
+    ros : ros,
+    name : '/chatter',
+    messageType : 'std_msgs/String'
+});
+
+var listener2 = new ROSLIB.Topic({
+    ros : ros,
+    name : '/chatter2',
+    messageType : 'std_msgs/String'
+});
+var str_1, str_2;
+
+listener.subscribe(function(m) {
+    str_1 = m.data;
+    document.getElementById("value_1").innerHTML = str_1;
+});
+
+imageTopic.subscribe(function(m) {
+var imagedata = "data:image/jpeg;base64," + m.data;
+document.getElementById('stream').setAttribute('src', imagedata);
+});
