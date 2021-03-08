81ee7404beb939e1a2262d6ce9e56db43d8d368a
Yunus Bayu Aji <yunus.bayu.aji@mail.ugm.ac.id>
2021-03-08 08:13:13 +0700

    combine with ros

--- web_js.js
+++ web_js.js
@@ -2,7 +2,7 @@ function validation(){
     var username = document.loginform.uname.value;
     var password = document.loginform.passwd.value;
     if(username == "ajietb" && password == "skripsi"){
-        window.location.href = "http://192.168.43.244/web_data.html"
+        window.location.href = "http://192.168.137.104/web_data.html"
         return true;
     }else if(username=="" && password==""){
         alert("Masukkan Username dan Password");
--- web_js2.js
+++ web_js2.js
@@ -1,5 +1,5 @@
 var ros = new ROSLIB.Ros({
-    url : 'ws://192.168.43.244:9090'
+    url : 'ws://192.168.137.104:9090'
 });
 
 ros.on('connection', function() {
@@ -14,38 +14,65 @@ ros.on('close', function() {
 console.log('Connection to websocket server closed.');
 });
 
-var imageTopic= new ROSLIB.Topic({
-ros : ros,
-name : '/image',
-messageType : 'sensor_msgs/CompressedImage'
+//======================================================================================================
+
+var imageSub = new ROSLIB.Topic({
+    ros : ros,
+    name : 'ayam/image',
+    messageType : 'sensor_msgs/CompressedImage'
 });
 
-var listener = new ROSLIB.Topic({
+var timeSub = new ROSLIB.Topic({
     ros : ros,
-    name : '/chatter',
+    name : 'ayam/time',
     messageType : 'std_msgs/String'
 });
 
-var listener2 = new ROSLIB.Topic({
+var targetSub = new ROSLIB.Topic({
     ros : ros,
-    name : '/chatter2',
+    name : 'ayam/target',
     messageType : 'std_msgs/String'
 });
-var str_1, str_2;
 
-listener.subscribe(function(m) {
-    str_1 = m.data;
-    document.getElementById("p_1").innerHTML = str_1;
+var errorSub = new ROSLIB.Topic({
+    ros : ros,
+    name : 'ayam/error',
+    messageType : 'std_msgs/String'
 });
 
-listener2.subscribe(function(m) {
-    str_2 = m.data;
-    document.getElementById("p_2").innerHTML = str_2;
+var detectSub = new ROSLIB.Topic({
+    ros : ros,
+    name : 'ayam/detect',
+    messageType : 'std_msgs/String'
 });
 
-imageTopic.subscribe(function(m) {
-var imagedata = "data:image/jpeg;base64," + m.data;
-document.getElementById('stream').setAttribute('src', imagedata);
+//======================================================================================================
+
+var timeData, targetData, errorData, detectData;
+
+timeSub.subscribe(function(m) {
+    timeData = m.data;
+    document.getElementById("p_1").innerHTML = timeData;
+});
+
+targetSub.subscribe(function(m) {
+    targetData = m.data;
+    document.getElementById("p_2").innerHTML = targetData;
+});
+
+errorSub.subscribe(function(m) {
+    errorData = m.data;
+    document.getElementById("p_3").innerHTML = errorData;
+});
+
+detectSub.subscribe(function(m) {
+    detectData = m.data;
+    document.getElementById("p_4").innerHTML = detectData;
+});
+
+imageSub.subscribe(function(m) {
+    var imagedata = "data:image/jpeg;base64," + m.data;
+    document.getElementById('stream').setAttribute('src', imagedata);
 });
 
 var state = 0
